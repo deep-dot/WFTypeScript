db47860c303839d5d0df45ce2acbaee165256a57
dee-dot <kuldeepdhillon_1982@yahoo.co.in>
2023-06-26 14:07:19 +1000

    .

--- PayingModule/Database/databaseService.tsx
+++ PayingModule/Database/databaseService.tsx
@@ -1,5 +1,33 @@
-import SQLite, {SQLiteDatabase} from 'react-native-sqlite-storage';
+import SQLite from 'react-native-sqlite-storage';
 
+type Transaction = {
+  executeSql: (
+    sql: string,
+    args?: any[],
+    success?: (transaction: Transaction, resultSet: ResultSet) => void,
+    error?: (transaction: Transaction, error: any) => void,
+  ) => void;
+};
+
+type ResultSet = {
+  rowsAffected: number;
+  insertId?: number;
+  rows: {
+    length: number;
+    item: (index: number) => any;
+    _array: any[];
+  };
+};
+
+interface SQLiteDatabase {
+  transaction: (callback: (transaction: Transaction) => void) => void;
+  executeSql: (
+    sqlStatement: string,
+    args?: any[],
+    callback?: (transaction: Transaction, resultSet: ResultSet) => void,
+    error?: (transaction: Transaction, error: any) => void,
+  ) => void;
+}
 let db: SQLiteDatabase | undefined;
 db = (SQLite.openDatabase as any)(
   {name: 'database.db', createFromLocation: 1},
